02936430de26df438a9200018d5692339988e1b8
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2020-03-26 06:05:28 +0200

    Initial Data

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
+	componentDidMount() {
+	  this.props.dispatch(handleInitialData()) // has access to the dispatch, because of the connect function
+	}
+
   render() {
     return (
       <div>
@@ -10,4 +16,5 @@ class App extends Component {
   }
 }
 
-export default App
+export default connect()(App)
+// Using the connect() function upgrades a component to a container. Containers can read state from the store and dispatch actions. 
