369886ecf98c2e1ac38a73c0dd8c2c6c177511a6
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2020-03-26 07:33:14 +0200

    QuestionsContainer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
+import QuestionsContainer from './QuestionsContainer'
 
 class App extends Component {
 	componentDidMount() {
@@ -9,9 +10,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        Starter Code
-      </div>
+      <QuestionsContainer />
     )
   }
 }
---
+++ src/components/QuestionsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function QuestionsContainer(props){
+  return (
+    <div className="app">
+    		Answered or Unanswered component
+    </div>
+  )
+}
+
+export default QuestionsContainer
