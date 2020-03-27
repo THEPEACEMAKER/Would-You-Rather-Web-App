96d13acbaa0a9cf22c897fa69ba0183bcd3a53d0
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2020-03-27 10:04:26 +0200

    add helper functions

---
+++ src/utils/helpers.js
@@ -0,0 +1,30 @@
+export function votesStat (votes1, votes2) {
+  const optionOneNum = votes1.length;
+  const optionTwoNum = votes2.length;
+  const total = optionOneNum + optionTwoNum;
+  const optionOnePercent = (100 * optionOneNum) / total;
+  const optionTwoPercent = (100 * optionTwoNum) / total;
+
+  return {
+    optionOneNum,
+    optionTwoNum,
+    total,
+    optionOnePercent,
+    optionTwoPercent
+  }
+}
+
+
+export function formatQuestion (question, author, authedUser) {
+  const { optionOne, optionTwo } = question
+  const { name, avatarURL } = author
+
+  return {
+    authorName: name,
+    authorAvatar: avatarURL,
+    optionOneText: optionOne.text,
+    optionTwoText: optionTwo.text,
+    votes: votesStat(optionOne.votes, optionTwo.votes),
+    answer: optionOne.votes.includes(authedUser)? 'optionOne' : (optionTwo.votes.includes(authedUser)? 'optionTwo' : null)
+  }
+}
